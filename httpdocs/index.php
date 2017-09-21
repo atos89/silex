9214ea6c9ccea68a19b8a3c68025170a4f33d79e
atos89 <atos990@gmail.com>
2017-09-21 22:27:56 +0900

    ルーティングを追加

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -3,6 +3,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app->get('/', function() {
-    return "Hello World!";
+    return 'Hello World!';
 });
-$app->run(); 
+
+$app->get('/hello/{name}', function($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+});
+
+$app->run();
